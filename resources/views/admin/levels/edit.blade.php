a8b93ab70946baed38edd12208d23e184bb55cbf
SondosAbdelhameed <sondos.abdelhameed@yahoo.com>
2023-07-10 14:44:47 +0200

    changes on dashboard add month, price, class price related to specific Level

--- resources/views/admin/levels/edit.blade.php
+++ resources/views/admin/levels/edit.blade.php
@@ -3,7 +3,7 @@
 <!-- Nav Header Component Start -->
 <x-dashboard.base.nav>
     <x-slot:heading>
-       المستويــات
+       المستوي - الثاني الثانوي
     </x-slot>
     {{-- We are on a mission to help developers like you build successful projects for FREE. --}}
 </x-dashboard.base.nav>
@@ -19,7 +19,7 @@
                 <div class="card">
                     <div class="card-header d-flex justify-content-between">
                         <div class="header-title">
-                            <h4 class="card-title">المستويــات</h4>
+                            <h4 class="card-title"></h4>
                         </div>
                     </div>
                     <div class="card-body px-0">
@@ -27,12 +27,10 @@
                             <div class="card">
                                 <div class="card-header d-flex justify-content-between">
                                     <div class="header-title">
-                                        <h4 class="card-title">تعديل المستوي</h4>
+                                        <h4 class="card-title">تعديل سعر الحصـــة</h4>
                                     </div>
                                 </div>
                                 <div class="card-body">
-                                    {{-- <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vulputate, ex ac
-                                        venenatis mollis, diam nibh finibus leo</p> --}}
                                     <form action="{{route('levels.update',$level->id)}}" method="post" enctype="multipart/form-data">
                                         @csrf
                                         @method('patch')
@@ -62,6 +60,91 @@
             </div>
         </div>
     </div>
+
+
+    <div>
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="card">
+                    <div class="card-header d-flex justify-content-between">
+                        <div class="header-title">
+                            <h4 class="card-title">بيانات الشهر</h4>
+                        </div>
+                    </div>
+                    <div class="card-body px-0">
+                        <div class="col-sm-12 col-lg-6">
+                            <div class="card">
+                                <div class="card-header d-flex justify-content-between">
+                                    <div class="header-title">
+                                        <h4 class="card-title">اضافة شهر</h4>
+                                    </div>
+                                </div>
+                                <div class="card-body">
+                                    <form action="{{route('level-store-month')}}" method="post" enctype="multipart/form-data">
+                                        @csrf
+                                        <input type="hidden" name="level_id" value="{{ $level->id }}">
+                                        <div class="form-group">
+                                            <label class="form-label" for="email">السعر :</label>
+                                            <input type="text" class="form-control" placeholder="السعر" name="price"
+                                                value="{{ old('price') }}" required />
+                                            @error('price')
+                                            <div class="alert alert-danger">{{ $message }}</div>
+                                            @enderror
+                                        </div>
+                                        <div class="form-group">
+                                            <label class="form-label">تاريخ البدأ :</label>
+                                            <input type="date" class="form-control" name="start_date"
+                                                value="{{ old('start_date') }}" required />
+                                            @error('start_date')
+                                            <div class="alert alert-danger">{{ $message }}</div>
+                                            @enderror
+                                        </div>
+                                        <div class="form-group">
+                                            <label class="form-label">تاريخ انتهاء :</label>
+                                            <input type="date" class="form-control" name="end_date"
+                                                value="{{ old('end_date') }}" required />
+                                            @error('end_date')
+                                            <div class="alert alert-danger">{{ $message }}</div>
+                                            @enderror
+                                        </div>
+                                        <button type="submit" class="btn btn-primary">حفظ</button>
+                                    </form>
+                                </div>
+                            </div>
+
+
+
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>السعر</th>
+                                        <th>تاريخ البداية</th>
+                                        <th>تاريخ انتهاء</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                     <tr>
+                                        <td>1</td>
+                                        <td>150</td>
+                                        <td>1-9-2023</td>
+                                        <td>30-9-2023</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+
+                        </div>
+
+
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+
+
 
 @endsection
--- resources/views/admin/levels/index.blade.php
+++ resources/views/admin/levels/index.blade.php
@@ -31,8 +31,8 @@
                                     <tr class="ligth">
                                         <th class="text-center">#</th>
                                         <th>اسم المستوي</th>
-                                        <th>سعر المستوي</th>
-                                        <th>الشهور</th>
+                                        <th>سعر الحصة</th>
+                                        <th>سعر الشهر الحالي</th>
                                         <th style="min-width: 100px">الإعدادات</th>
                                     </tr>
                                 </thead>
@@ -41,34 +41,28 @@
                                     <tr>
                                         <td class="text-center">{{ $index+1 }}</td>
                                         <td>{{ $level->name }}</td>
-                                        <td>{{ $level->one_price }}</td>
+                                        <td>{{ $level->one_price }} ج</td>
                                         <td>
-                                            <div class="iq-media-group iq-media-group-1">
-                                                @foreach ($level->monthlyLevelPrices as $month)
-                                                <a href="{{ route('month-details', $month->id) }}" class="iq-media-1">
-                                                    <div class="icon iq-icon-box-3 rounded-pill">{{ $month->start_date->format('M') }}</div>
-                                                </a>
-                                                @endforeach
-                                            </div>
+                                            150 ج
                                         </td>
                                         <td>
                                             <div class="flex align-items-center list-user-action"
                                                 style="display: flex;">
-                                                <x-dashboard.a-edit href="{{ route('levels.edit', $level->id) }}">
-                                                </x-dashboard.a-edit>&nbsp;
+                                                <!--<x-dashboard.a-edit href="{{ route('levels.edit', $level->id) }}">
+                                                </x-dashboard.a-edit>&nbsp;-->
                                                 {{-- <form action="{{ route('levels.destroy', $level->id) }}" method="POST">
                                                     @csrf
                                                     @method('DELETE')
                                                     <x-dashboard.delete-button></x-dashboard.delete-button>
                                                 </form>&nbsp; --}}
                                                 <a class="btn btn-sm btn-icon btn-success"
-                                                    href="{{ route('level-create-month', $level->id) }}"
-                                                    title="اضافة شهر جديد">
-                                                    <svg width="20" viewBox="0 0 24 24" fill="none"
-                                                        xmlns="http://www.w3.org/2000/svg">
-                                                        <path fill-rule="evenodd" clip-rule="evenodd"
-                                                            d="M16.4109 2.76862L16.4119 3.51824C19.1665 3.73413 20.9862 5.61119 20.9891 8.48975L21 16.9155C21.0039 20.054 19.0322 21.985 15.8718 21.99L8.15188 22C5.01119 22.004 3.01482 20.027 3.01087 16.8795L3.00001 8.55272C2.99606 5.65517 4.75153 3.78311 7.50617 3.53024L7.50518 2.78061C7.5042 2.34083 7.83001 2.01 8.26444 2.01C8.69886 2.009 9.02468 2.33883 9.02567 2.77861L9.02666 3.47826L14.8914 3.47027L14.8904 2.77062C14.8894 2.33084 15.2152 2.001 15.6497 2C16.0742 1.999 16.4099 2.32884 16.4109 2.76862ZM4.52148 8.86157L19.4696 8.84158V8.49175C19.4272 6.34283 18.349 5.21539 16.4138 5.04748L16.4148 5.81709C16.4148 6.24688 16.0801 6.5877 15.6556 6.5877C15.2212 6.5887 14.8943 6.24887 14.8943 5.81909L14.8934 5.0095L9.02863 5.01749L9.02962 5.82609C9.02962 6.25687 8.70479 6.5967 8.27036 6.5967C7.83594 6.5977 7.50913 6.25887 7.50913 5.82809L7.50815 5.05847C5.58286 5.25137 4.51753 6.38281 4.52049 8.55072L4.52148 8.86157ZM15.2399 13.4043V13.4153C15.2498 13.8751 15.625 14.2239 16.0801 14.2139C16.5244 14.2029 16.8789 13.8221 16.869 13.3623C16.8483 12.9225 16.4918 12.5637 16.0485 12.5647C15.5944 12.5747 15.2389 12.9445 15.2399 13.4043ZM16.0554 17.892C15.6013 17.882 15.235 17.5032 15.234 17.0435C15.2241 16.5837 15.5884 16.2029 16.0426 16.1919H16.0525C16.5165 16.1919 16.8927 16.5707 16.8927 17.0405C16.8937 17.5102 16.5185 17.891 16.0554 17.892ZM11.1721 13.4203C11.1919 13.8801 11.568 14.2389 12.0222 14.2189C12.4665 14.1979 12.821 13.8181 12.8012 13.3583C12.7903 12.9085 12.425 12.5587 11.9807 12.5597C11.5266 12.5797 11.1711 12.9605 11.1721 13.4203ZM12.0262 17.8471C11.572 17.8671 11.1968 17.5082 11.1761 17.0485C11.1761 16.5887 11.5305 16.2089 11.9847 16.1879C12.429 16.1869 12.7953 16.5367 12.8052 16.9855C12.8259 17.4463 12.4705 17.8261 12.0262 17.8471ZM7.10433 13.4553C7.12408 13.915 7.50025 14.2749 7.95442 14.2539C8.39872 14.2339 8.75317 13.8531 8.73243 13.3933C8.72256 12.9435 8.35725 12.5937 7.91196 12.5947C7.45779 12.6147 7.10334 12.9955 7.10433 13.4553ZM7.95837 17.8521C7.5042 17.8731 7.12901 17.5132 7.10828 17.0535C7.10729 16.5937 7.46273 16.2129 7.9169 16.1929C8.3612 16.1919 8.7275 16.5417 8.73737 16.9915C8.7581 17.4513 8.40365 17.8321 7.95837 17.8521Z"
-                                                            fill="currentColor"></path>
+                                                    href="{{ route('levels.edit', $level->id) }}"
+                                                    title=" اضافة وتعديل سعر الحصة والشهر">
+                                                    <svg width="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                        <path d="M15.7161 16.2234H8.49609" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                                                        <path d="M15.7161 12.0369H8.49609" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                                                        <path d="M11.2521 7.86011H8.49707" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                                                        <path fill-rule="evenodd" clip-rule="evenodd" d="M15.909 2.74976C15.909 2.74976 8.23198 2.75376 8.21998 2.75376C5.45998 2.77076 3.75098 4.58676 3.75098 7.35676V16.5528C3.75098 19.3368 5.47298 21.1598 8.25698 21.1598C8.25698 21.1598 15.933 21.1568 15.946 21.1568C18.706 21.1398 20.416 19.3228 20.416 16.5528V7.35676C20.416 4.57276 18.693 2.74976 15.909 2.74976Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
                                                     </svg>
                                                 </a>
                                             </div>
